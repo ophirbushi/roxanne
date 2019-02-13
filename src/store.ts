d4bbda697f7e29bddf8a520c15cb99f518523957
Ophir Bushinsky <ophirbushi@gmail.com>
2019-02-14 01:15:12 +0200

    restore support for effects as a function, or array of functions

--- src/store.ts
+++ src/store.ts
@@ -8,9 +8,21 @@ export class Store<State, Actions> extends BehaviorSubject<State> {
 
     constructor(
         initialState: State,
-        private reducer: Reducer<State, Actions>
+        private reducer: Reducer<State, Actions>,
+        private effects?: (store: Store<State, Actions>) => void | Array<(store: Store<State, Actions>) => void>
     ) {
         super(initialState);
+
+        if (Array.isArray(effects)) {
+            effects.forEach(effect => {
+                if (typeof effect !== 'function') {
+                    throw new Error('Effects need to be a function, or an array of functions.');
+                }
+                effect(this);
+            });
+        } else if (typeof effects === 'function') {
+            effects(this);
+        }
     }
 
     dispatch<ActionType extends keyof Actions>(action: ActionType, payload: Actions[ActionType]) {
@@ -29,12 +41,11 @@ export class Store<State, Actions> extends BehaviorSubject<State> {
             );
     }
 
-     actionOfType<K extends keyof Actions>(action: K): Observable<Actions[K]> {
+    actionOfType<K extends keyof Actions>(action: K): Observable<Actions[K]> {
         return this._actions$
             .pipe(
                 filter(couple => couple.action === action),
                 map(couple => <Actions[K]>couple.payload)
             );
     }
-    
 }
